ff929d6b6a9b2e8e2650e99fabf4b10b630de4d3
damilare-d <damilareakinwale0@gmail.com>
2024-06-13 19:45:36 +0100

    api integration: sign up integration works

--- lib/ui/screens/sign_up/sign_up_viewmodel.dart
+++ lib/ui/screens/sign_up/sign_up_viewmodel.dart
@@ -1,25 +1,62 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:smartpay/app/app.locator.dart';
 import 'package:smartpay/app/app.router.dart';
-import 'package:smartpay/app/application.dart';
+import 'package:smartpay/core/models/email_token_model.dart';
+import 'package:smartpay/core/services/auth_repository.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
 
-class SignUpViewModel extends BaseViewModel{
-
+class SignUpViewModel extends BaseViewModel {
   final _navigationService = locator<NavigationService>();
+  final _bottomSheetService = locator<BottomSheetService>();
+  final _authRepository = locator<AuthRepository>();
+
   TextEditingController emailController = TextEditingController();
+  EmailTokenResponse? emailTokenResponse;
+
+  Future<void> requestEmailToken(String email) async {
+    setBusy(true);
+    try {
+      emailTokenResponse = await _authRepository.requestEmailToken(email);
+      if (emailTokenResponse?.status == true) {
+        _navigationService.navigateTo(Routes.authenticationView, arguments: emailController.text);
+      } else {
+        _bottomSheetService.showBottomSheet(
+          title: 'Error',
+          description: emailTokenResponse?.message ?? 'Unknown error occurred',
+
+        );
+      }
+    } catch (e) {
+      _bottomSheetService.showBottomSheet(
+        title: 'Error',
+        description: 'Error requesting email token: $e',
+
+      );
+    } finally {
+      setBusy(false);
+      notifyListeners();
+    }
+  }
 
   void signUp() {
-    _navigationService.navigateTo(Routes.authenticationView);
-
+    requestEmailToken(emailController.text);
   }
 
   void googleAuth() {
+    _bottomSheetService.showBottomSheet(
+      title: '....',
+      description: 'Upcoming feature, stay smart',
+
+    );
+
   }
 
   void appleAuth() {
+    _bottomSheetService.showBottomSheet(
+      title: '....',
+      description: 'Upcoming feature, stay smart',
+    );
   }
 
   void signIn() {
