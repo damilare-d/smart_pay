ef148c29cd6ec87c98c4ebfbfc7483e7b849ce78
damilare-d <damilareakinwale0@gmail.com>
2024-06-13 15:52:14 +0100

    api integration: changing api_service to use http instead of dio

--- lib/core/services/api_service.dart
+++ lib/core/services/api_service.dart
@@ -1,27 +1,79 @@
-import 'package:dio/dio.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:smartpay/core/models/models.dart';
 
-
 class ApiService {
-  final Dio _dio = Dio(BaseOptions(baseUrl: 'https://mobile-test-2d7e555a4f85.herokuapp.com/api/v1'));
+  final String _baseUrl = 'https://mobile-test-2d7e555a4f85.herokuapp.com/api/v1';
 
   Future<EmailTokenResponse> requestEmailToken(EmailTokenRequest request) async {
-    final response = await _dio.post('/api/v1/auth/email', data: request.toJson());
-    return EmailTokenResponse.fromJson(response.data);
+    final url = Uri.parse('$_baseUrl/auth/email');
+    final response = await http.post(
+      url,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(request.toJson()),
+    );
+
+    if (response.statusCode == 200) {
+      return EmailTokenResponse.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to request email token: ${response.reasonPhrase}');
+    }
   }
 
   Future<VerifyEmailTokenResponse> verifyEmailToken(VerifyEmailTokenRequest request) async {
-    final response = await _dio.post('/api/v1/auth/email/verify', data: request.toJson());
-    return VerifyEmailTokenResponse.fromJson(response.data);
+    final url = Uri.parse('$_baseUrl/auth/email/verify');
+    final response = await http.post(
+      url,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(request.toJson()),
+    );
+
+    if (response.statusCode == 200) {
+      return VerifyEmailTokenResponse.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to verify email token: ${response.reasonPhrase}');
+    }
   }
 
   Future<RegisterResponse> register(RegisterRequest request) async {
-    final response = await _dio.post('/api/v1/auth/register', data: request.toJson());
-    return RegisterResponse.fromJson(response.data);
+    final url = Uri.parse('$_baseUrl/auth/register');
+    final response = await http.post(
+      url,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(request.toJson()),
+    );
+
+    if (response.statusCode == 200) {
+      return RegisterResponse.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to register: ${response.reasonPhrase}');
+    }
   }
 
   Future<LoginResponse> login(LoginRequest request) async {
-    final response = await _dio.post('/api/v1/auth/login', data: request.toJson());
-    return LoginResponse.fromJson(response.data);
+    final url = Uri.parse('$_baseUrl/auth/login');
+    final response = await http.post(
+      url,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(request.toJson()),
+    );
+
+    if (response.statusCode == 200) {
+      return LoginResponse.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to login: ${response.reasonPhrase}');
+    }
   }
 }
