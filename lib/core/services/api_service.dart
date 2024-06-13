534d2ae3ca29df128e9e1da1074b65ae9febb1d8
damilare-d <damilareakinwale0@gmail.com>
2024-06-13 16:14:58 +0100

    api integration: api service methods updated with try catch block

--- lib/core/services/api_service.dart
+++ lib/core/services/api_service.dart
@@ -6,74 +6,90 @@ class ApiService {
   final String _baseUrl = 'https://mobile-test-2d7e555a4f85.herokuapp.com/api/v1';
 
   Future<EmailTokenResponse> requestEmailToken(EmailTokenRequest request) async {
-    final url = Uri.parse('$_baseUrl/auth/email');
-    final response = await http.post(
-      url,
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: jsonEncode(request.toJson()),
-    );
+    try {
+      final url = Uri.parse('$_baseUrl/auth/email');
+      final response = await http.post(
+        url,
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(request.toJson()),
+      );
 
-    if (response.statusCode == 200) {
-      return EmailTokenResponse.fromJson(jsonDecode(response.body));
-    } else {
-      throw Exception('Failed to request email token: ${response.reasonPhrase}');
+      if (response.statusCode == 200) {
+        return EmailTokenResponse.fromJson(jsonDecode(response.body));
+      } else {
+        throw Exception('Failed to request email token: ${response.reasonPhrase}');
+      }
+    } catch (e) {
+      throw Exception('Error requesting email token: $e');
     }
   }
 
   Future<VerifyEmailTokenResponse> verifyEmailToken(VerifyEmailTokenRequest request) async {
-    final url = Uri.parse('$_baseUrl/auth/email/verify');
-    final response = await http.post(
-      url,
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: jsonEncode(request.toJson()),
-    );
+    try {
+      final url = Uri.parse('$_baseUrl/auth/email/verify');
+      final response = await http.post(
+        url,
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(request.toJson()),
+      );
 
-    if (response.statusCode == 200) {
-      return VerifyEmailTokenResponse.fromJson(jsonDecode(response.body));
-    } else {
-      throw Exception('Failed to verify email token: ${response.reasonPhrase}');
+      if (response.statusCode == 200) {
+        return VerifyEmailTokenResponse.fromJson(jsonDecode(response.body));
+      } else {
+        throw Exception('Failed to verify email token: ${response.reasonPhrase}');
+      }
+    } catch (e) {
+      throw Exception('Error verifying email token: $e');
     }
   }
 
   Future<RegisterResponse> register(RegisterRequest request) async {
-    final url = Uri.parse('$_baseUrl/auth/register');
-    final response = await http.post(
-      url,
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: jsonEncode(request.toJson()),
-    );
+    try {
+      final url = Uri.parse('$_baseUrl/auth/register');
+      final response = await http.post(
+        url,
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(request.toJson()),
+      );
 
-    if (response.statusCode == 200) {
-      return RegisterResponse.fromJson(jsonDecode(response.body));
-    } else {
-      throw Exception('Failed to register: ${response.reasonPhrase}');
+      if (response.statusCode == 200) {
+        return RegisterResponse.fromJson(jsonDecode(response.body));
+      } else {
+        throw Exception('Failed to register: ${response.reasonPhrase}');
+      }
+    } catch (e) {
+      throw Exception('Error registering: $e');
     }
   }
 
   Future<LoginResponse> login(LoginRequest request) async {
-    final url = Uri.parse('$_baseUrl/auth/login');
-    final response = await http.post(
-      url,
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: jsonEncode(request.toJson()),
-    );
+    try {
+      final url = Uri.parse('$_baseUrl/auth/login');
+      final response = await http.post(
+        url,
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(request.toJson()),
+      );
 
-    if (response.statusCode == 200) {
-      return LoginResponse.fromJson(jsonDecode(response.body));
-    } else {
-      throw Exception('Failed to login: ${response.reasonPhrase}');
+      if (response.statusCode == 200) {
+        return LoginResponse.fromJson(jsonDecode(response.body));
+      } else {
+        throw Exception('Failed to login: ${response.reasonPhrase}');
+      }
+    } catch (e) {
+      throw Exception('Error logging in: $e');
     }
   }
 }
