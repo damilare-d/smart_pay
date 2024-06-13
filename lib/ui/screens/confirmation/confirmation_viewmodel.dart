6d1f58bac88786bbfa0e592b54f3a17712bb4a0c
damilare-d <damilareakinwale0@gmail.com>
2024-06-13 23:42:14 +0100

    api integration : api integrated with try catch block and bottom sheet

--- lib/ui/screens/confirmation/confirmation_viewmodel.dart
+++ lib/ui/screens/confirmation/confirmation_viewmodel.dart
@@ -1,12 +1,41 @@
-
-
 import 'package:smartpay/app/app.router.dart';
 import 'package:smartpay/app/application.dart';
+import 'package:smartpay/core/models/login_model.dart';
+import 'package:smartpay/core/services/auth_repository.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+import 'package:smartpay/core/services/user_details_service.dart';
 
-class ConfirmationViewModel extends BaseViewModel{
-
+class ConfirmationViewModel extends BaseViewModel {
   final _navigationService = locator<NavigationService>();
+  final _authRepository = locator<AuthRepository>();
+  final _bottomSheetService = locator<BottomSheetService>();
+  final _userDetailsService = locator<UserDetailsService>();
+  LoginResponse? loginResponse;
+
+  Future<void> login() async {
+    setBusy(true);
+    try {
+      loginResponse = await _authRepository.login(_userDetailsService.email,
+          _userDetailsService.password, "phone");
+      if (loginResponse?.status == true) {
+        navigateToHome();
+      } else {
+        await _bottomSheetService.showBottomSheet(
+          title: 'Login Failed',
+          description: loginResponse?.message ?? 'Failed to login',
+        );
+      }
+    } catch (e) {
+      await _bottomSheetService.showBottomSheet(
+        title: 'Error',
+        description: 'Error logging in: $e',
+      );
+    } finally {
+      setBusy(false);
+      notifyListeners();
+    }
+  }
 
   void navigateToHome() {
     _navigationService.navigateTo(Routes.homeView);
