7df67cd31d4ddd98ad374c68db94ff834f31932b
damilare-d <damilareakinwale0@gmail.com>
2024-06-14 01:17:41 +0100

    bottom shhet

--- lib/app/app.bottomsheets.dart
+++ lib/app/app.bottomsheets.dart
@@ -7,10 +7,12 @@
 import 'package:stacked_services/stacked_services.dart';
 
 import 'app.locator.dart';
+import '../ui/bottom_sheets/country_selwcwtion_sheet/country_selection_sheet.dart';
 import '../ui/bottom_sheets/notice/notice_sheet.dart';
 
 enum BottomSheetType {
   notice,
+  countrySelection,
 }
 
 void setupBottomSheetUi() {
@@ -19,6 +21,8 @@ void setupBottomSheetUi() {
   final Map<BottomSheetType, SheetBuilder> builders = {
     BottomSheetType.notice: (context, request, completer) =>
         NoticeSheet(request: request, completer: completer),
+    BottomSheetType.countrySelection: (context, request, completer) =>
+        CountrySelectionSheet(request: request, completer: completer),
   };
 
   bottomsheetService.setCustomSheetBuilders(builders);
